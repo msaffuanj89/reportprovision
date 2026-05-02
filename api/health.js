1535a7c6d6e98e1aff11a7606d39bfd409290215
msaffuanj89 <saffuan.jaffar@ipgm.edu.my>
2026-05-02 09:12:58 +0800

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,5 +1,3 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true }));
+  res.status(200).json({ ok: true });
 }
